a6e0670149ee8cdc4076e15777db47bad0c8599b
patriki28 <diestapatrick1@gmail.com>
2023-03-06 15:55:25 +0800

    Added a chat room that can join and leave functionality and a notification that notify other usser if someone joined the room or leave the room

--- Omegle-Chat/Hubs/ChatHub.cs
+++ Omegle-Chat/Hubs/ChatHub.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
 
 namespace SignalRChat.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string user, string message, string room, bool join)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            if (join)
+            {
+                await JoinRoom(room).ConfigureAwait(false);
+                await Clients.Group(room).SendAsync("ReceiveMessage", user, " joined to " + room, true).ConfigureAwait(true);   
+            }
+            else
+            {
+                await Clients.Group(room).SendAsync("ReceiveMessage", user, message, false).ConfigureAwait(true);
+            }
+        }
+
+        public Task JoinRoom(string roomName)
+        {
+            return Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+        }
+        public async Task LeaveChatRoom(string user, string room)
+        {
+            await Clients.Group(room).SendAsync("LeaveNotif", user, "left the room.", false).ConfigureAwait(true);
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, room).ConfigureAwait(false);
         }
     }
 }
--- Omegle-Chat/wwwroot/js/chat.js
+++ Omegle-Chat/wwwroot/js/chat.js
@@ -4,11 +4,29 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
 document.getElementById("sendButton").disabled = true;
 
-connection.on("ReceiveMessage", function (user, message) {
-    var li = document.createElement("li");
-    document.getElementById("messagesList").appendChild(li);
+connection.on("ReceiveMessage", function (user, message, isJoin) {
+    if (isJoin) {
+        alert(user + " joined the room!");
+    }
+    else {
+        var div = document.createElement("div");
+        if (user === document.getElementById("userInput").value) {
+            div.classList.add("sent-message");
+        } else {
+            div.classList.add("received-message");
+        }
+        document.getElementById("messagesList").appendChild(div);
 
-    li.textContent = `${user} says ${message}`;
+        var name = document.createElement("div");
+        name.classList.add("message-name");
+        name.textContent = user;
+        div.appendChild(name);
+
+        var text = document.createElement("div");
+        text.classList.add("message-text");
+        text.textContent = message;
+        div.appendChild(text);
+    }
 });
 
 connection.start().then(function () {
@@ -20,8 +38,46 @@ connection.start().then(function () {
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
+    var room = document.getElementById("room").value;
+    connection.invoke("SendMessage", user, message, room, false).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
 });
+
+document.getElementById("joinButton").addEventListener("click", function (event) {
+
+    var room = document.getElementById("room").value;
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message, room, true).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+document.getElementById("leaveButton").addEventListener("click", function (event) {
+    connection.invoke("LeaveRoom", room).catch(function (err) {
+        return console.error(err.toString());
+    });
+
+    // reset the message list
+    var messagesList = document.getElementById("messagesList");
+    while (messagesList.firstChild) {
+        messagesList.removeChild(messagesList.firstChild);
+    }
+
+    event.preventDefault();
+});
+
+window.onbeforeunload = function (event) {
+    var room = document.getElementById("room").value;
+    var user = document.getElementById("userInput").value;
+    connection.invoke("SendMessage", user, "left the room.", room, false).catch(function (err) {
+        return console.error(err.toString());
+    });
+
+    var confirmationMessage = 'Are you sure you want to leave the room?';
+    event.returnValue = confirmationMessage;
+    return confirmationMessage;
+};
